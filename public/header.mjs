c711c57917d9b63fbd8a335b91766aa7179e0f85
Ziadin Navarro <ziadinnavarro4@gmail.com>
2025-05-05 03:40:30 -0700

    initial header stuff

--- public/header.mjs
+++ public/header.mjs
@@ -1,19 +1,36 @@
+const headerTitle = "Ziadin";
 const routes = [
   { title: "Home Page", path: "./index.html" },
   { title: "Another Page", path: "./another-page.html" },
 ];
-
-let isVisibleNavPaths = false;
-
 const navPaths = routes
   .map((route) => `<a href="${route.path}">${route.title}</a>`)
   .join("");
+const initialHeaderContent = `
+  <div class="header-content">
+    <h1>${headerTitle}</h1>
+    <button id="burger-button">
+      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#ffffff" viewBox="0 0 256 256">
+        <path
+          d="M224,128a8,8,0,0,1-8,8H40a8,8,0,0,1,0-16H216A8,8,0,0,1,224,128ZM40,72H216a8,8,0,0,0,0-16H40a8,8,0,0,0,0,16ZM216,184H40a8,8,0,0,0,0,16H216a8,8,0,0,0,0-16Z">
+        </path>
+      </svg>
+    </button>
+  </div>
+  <nav>
+    ${navPaths}
+  </nav>
+`;
 
-const nav = document.querySelector("header nav");
-nav.innerHTML = navPaths;
+let isVisibleNavPaths = false;
+const body = document.querySelector("body");
+const header = document.createElement("header");
+header.innerHTML = initialHeaderContent;
+body.prepend(header);
 
 const burgerButton = document.querySelector("#burger-button");
 burgerButton.addEventListener("click", () => {
+  const nav = document.querySelector("header nav");
   isVisibleNavPaths = !isVisibleNavPaths;
   nav.style.display = isVisibleNavPaths ? "flex" : "none";
 });
