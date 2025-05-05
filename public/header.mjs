c29361bff908dc0ede1e3837700373763877f0fa
Ziadin Navarro <ziadinnavarro4@gmail.com>
2025-05-05 03:24:03 -0700

    initial burger menu working

--- public/header.mjs
+++ public/header.mjs
@@ -1,1 +1,19 @@
-console.log("Hello");
+const routes = [
+  { title: "Home Page", path: "./index.html" },
+  { title: "Another Page", path: "./another-page.html" },
+];
+
+let isVisibleNavPaths = false;
+
+const navPaths = routes
+  .map((route) => `<a href="${route.path}">${route.title}</a>`)
+  .join("");
+
+const nav = document.querySelector("header nav");
+nav.innerHTML = navPaths;
+
+const burgerButton = document.querySelector("#burger-button");
+burgerButton.addEventListener("click", () => {
+  isVisibleNavPaths = !isVisibleNavPaths;
+  nav.style.display = isVisibleNavPaths ? "flex" : "none";
+});
